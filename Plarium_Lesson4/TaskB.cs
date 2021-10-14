b355efca534a7446c2c1b84868ff3ebe3b949c57
lyolyabond <olha.bondarenko@nure.ua>
2021-10-14 10:48:38 +0300

    Add project files.

---
+++ Plarium_Lesson4/TaskB.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace Plarium_Lesson4
+{
+    class TaskB
+    {
+         static void FillTupleCircle( out(double xCentre, double yCentre, double radius) circle)
+        {
+            Console.WriteLine("--Введите координаты центра окружности--");
+            Console.Write("\tx: ");
+            circle.xCentre = double.Parse(Console.ReadLine());
+            Console.Write("\ty: ");
+            circle.yCentre = double.Parse(Console.ReadLine());
+            Console.Write("--Введите радиус окружности: ");
+            circle.radius = double.Parse(Console.ReadLine());
+        }
+        static void FillTuplePoint(out (double x, double y) point)
+        {
+            Console.WriteLine("--Введите координаты точки--");
+            Console.Write("\tx: ");
+            point.x = double.Parse(Console.ReadLine());
+            Console.Write("\ty: ");
+            point.y = double.Parse(Console.ReadLine());
+        }
+        static void HittingPoint((double xCentre, double yCentre, double radius) circle, (double x, double y) point)
+        {
+            if (Math.Pow((point.x - circle.xCentre), 2) + Math.Pow((point.y - circle.yCentre), 2) <= Math.Pow(circle.radius, 2))
+            {
+                Console.WriteLine("~Точка попадает в данную окружность~");
+            }
+            else Console.WriteLine("~Точка не попадает в данную окружность~");
+        }
+
+        static double Distance((double xCentre, double yCentre, double radius) circle,
+                                (double xCentre, double yCentre, double radius) circle1)
+           => Math.Sqrt(Math.Pow((circle1.xCentre - circle.xCentre), 2) + Math.Pow((circle1.yCentre - circle.yCentre), 2));
+             
+
+        static void HittingCircle((double xCentre, double yCentre, double radius) circle, 
+                                (double xCentre, double yCentre, double radius) circle1)
+        {
+            double distance = Distance(circle, circle1);
+            if (distance + circle1.radius <= circle.radius)
+                Console.WriteLine("~Круг попадает в данную окружность~");
+            else Console.WriteLine("~Круг не попадает в данную окружность~");
+        }
+
+        static void CrossingCircle((double xCentre, double yCentre, double radius) circle,
+                                (double xCentre, double yCentre, double radius) circle1)
+        {
+            double distance = Distance(circle, circle1);
+            if (distance !=0  &&
+                distance >= Math.Abs(circle.radius- circle1.radius))
+                {
+                Console.WriteLine("~Круг пересекает данную окружность~");
+                }
+            else Console.WriteLine("~Круг не пересекает данную окружность~");
+        }
+
+
+        static void Main(string[] args)
+        {
+            (double xCentre, double yCentre, double radius) circle;
+            (double x, double y) point;
+
+            FillTupleCircle(out circle);
+            FillTuplePoint(out point);
+            HittingPoint(circle, point);
+
+            (double xCentre, double yCentre, double radius) circle1;
+            FillTupleCircle(out circle1);
+            HittingCircle(circle, circle1);
+            CrossingCircle(circle, circle1);
+
+         
+        }
+    }
+}
---
+++ TaskC/Program.cs
@@ -0,0 +1,216 @@
+﻿using System;
+
+namespace TaskC
+{
+    class Program
+    {
+        public struct Manufacturer
+        {
+            public Manufacturer(string name, string country)
+            {
+               Name = name;
+               Country = country;
+            }
+            public string Name { get; }
+            public string Country { get; }
+
+            /*public void DisplayInformationManufacturer()
+            {
+                Console.WriteLine($"Название производителя: {Name}");
+                Console.WriteLine($"Страна производителя: {Country}");
+            }*/
+        }
+
+        public struct Souvenir
+        {
+            public Manufacturer manufacturer;
+            public Souvenir(string souvenirName, string manufacturerRequisites, DateTime releaseDate, decimal price, Manufacturer someManufacturer) 
+            {
+                SouvenirName = souvenirName;
+                ManufacturerRequisites = manufacturerRequisites;
+                ReleaseDate = releaseDate;
+                Price = price;
+                manufacturer = someManufacturer;
+            }
+            public string SouvenirName { get;}
+            public string ManufacturerRequisites { get; }
+            public DateTime ReleaseDate { get; }
+            public decimal Price { get; }
+            public void DisplayInformationSouvenir()
+            {
+                Console.WriteLine($"Название сувенира: {SouvenirName}");
+                Console.WriteLine($"Реквизиты производителя: {ManufacturerRequisites}");
+                Console.WriteLine($"Дата выпуска: {ReleaseDate.ToShortDateString()}");
+                Console.WriteLine($"Цена: {Price}");
+                Console.WriteLine("--------------------------"); 
+            }
+
+            public void DisplayInformationManufacturer()
+            {
+                Console.WriteLine($"Название производителя: {manufacturer.Name}");
+                Console.WriteLine($"Страна производителя: {manufacturer.Country}");
+            }
+
+        }
+        public static Souvenir EnterInformation()
+        {
+            string souvenirName;
+            string manufacturerRequisites;
+            DateTime releaseDate;
+            decimal price;
+            string name;
+            string country;
+            //DateTime dtOut;
+
+            Console.Write("Введите название сувенира: ");
+            souvenirName = Console.ReadLine();
+            Console.Write("Введите реквизиты производителя: ");
+            manufacturerRequisites = Console.ReadLine();
+
+            Console.Write("Введите дату выпуска: ");
+            while (!DateTime.TryParse(Console.ReadLine(), out releaseDate))
+            {
+                Console.Write("Введите дату в формате: {0:d} ", new DateTime(2008, 1, 7));
+            }
+            Console.Write("Введите цену: ");
+            price = decimal.Parse(Console.ReadLine());
+
+            Console.Write("Введите название производителя: ");
+            name = Console.ReadLine();
+            Console.Write("Введите страну производителя: ");
+            country = Console.ReadLine();
+            Console.WriteLine("--------------------------");
+            Manufacturer manufacturer = new Manufacturer(name, country);
+             Souvenir  souvenir = new Souvenir(souvenirName, manufacturerRequisites, releaseDate, price, manufacturer);
+            return souvenir;
+        }
+        /*class Manufacturer
+         {
+             public Manufacturer(string name, string country)
+             {
+                 Name = name;
+                 Country = country;
+             }
+             public  string Name { get; }
+             public  string Country { get; }
+         }
+         class Souvenir
+         {
+              Manufacturer manufacturer1;
+             public Souvenir(string souvenirName, string manufacturerRequisites, DateTime releaseDate, decimal price, Manufacturer someManufacturer)
+             {
+                 SouvenirName = souvenirName;
+                 ManufacturerRequisites = manufacturerRequisites;
+                 ReleaseDate = releaseDate;
+                 Price = price;
+                 manufacturer1 = someManufacturer;
+             }
+             public string SouvenirName { get; }
+             public string ManufacturerRequisites { get; }
+             public DateTime ReleaseDate { get; }
+             public decimal Price { get; }
+
+
+         }*/
+        public static Souvenir[] AddingItem()
+        {
+            Console.Write("Введите сколько записей о сувенирах вы хотите создать: ");
+            int number = int.Parse(Console.ReadLine());
+            
+            Souvenir[] arraySouvenir = new Souvenir[number];
+            for(int i = 0; i < number; i++)
+            {
+                Console.WriteLine($"\t{i + 1}-элемент");
+                arraySouvenir[i] = EnterInformation();
+            }
+            return arraySouvenir;
+
+        }
+        public static void DisplayInformationByManufacturer(Souvenir[] arraySouvenir)
+        {
+            Console.Write("Введите название производителя: ");
+            string name = Console.ReadLine();
+            for(int i = 0; i< arraySouvenir.Length; i++)
+            {
+                if(arraySouvenir[i].manufacturer.Name == name)
+                {
+                    arraySouvenir[i].DisplayInformationSouvenir();
+                }
+            }
+        }
+        public static void DisplayInformationByCountry(Souvenir[] arraySouvenir)
+        {
+            Console.Write("Введите название страны: ");
+            string country = Console.ReadLine();
+            for (int i = 0; i < arraySouvenir.Length; i++)
+            {
+                if (arraySouvenir[i].manufacturer.Country == country)
+                {
+                    arraySouvenir[i].DisplayInformationSouvenir();
+                }
+            }
+        }
+
+        public static void DisplayInformationByPrice(Souvenir[] arraySouvenir)
+        {
+            Console.Write("Введите цену: ");
+            decimal price = decimal.Parse(Console.ReadLine());
+            for (int i = 0; i < arraySouvenir.Length; i++)
+            {
+                if (arraySouvenir[i].Price < price)
+                {
+                    arraySouvenir[i].DisplayInformationManufacturer();
+                }
+            }
+        }
+        public static void DisplayInformationByDate(Souvenir[] arraySouvenir)
+        {
+            DateTime releaseDate;
+            Console.Write("Введите дату выпуска: ");
+            while (!DateTime.TryParse(Console.ReadLine(), out releaseDate))
+            {
+                Console.Write("Введите дату в формате: {0:d} ", new DateTime(2008, 1, 7));
+            }
+            for (int i = 0; i < arraySouvenir.Length; i++)
+            {
+                if (arraySouvenir[i].ReleaseDate == releaseDate)
+                {
+                    arraySouvenir[i].DisplayInformationManufacturer();
+                }
+            }
+        }
+        public static void DeleteItemByManufacturer(ref Souvenir[] arraySouvenir)
+        {
+            Console.Write("Введите название производителя: ");
+            string name = Console.ReadLine();
+            int count = 0;
+            for (int i = 0; i < arraySouvenir.Length; i++)
+            {
+                if (arraySouvenir[i].manufacturer.Name == name)
+                {
+                    count++;
+                    for (int j = i; j < arraySouvenir.Length-count; j++)
+                    {
+                        arraySouvenir[j] = arraySouvenir[j + 1];
+                    }
+                }
+                
+            }
+        Array.Resize(ref arraySouvenir, arraySouvenir.Length - count);
+        }
+        static void Main(string[] args)
+        {
+            Souvenir[] arraySouvenir = AddingItem();
+            DeleteItemByManufacturer(ref arraySouvenir);
+
+            for (int i = 0; i < arraySouvenir.Length; i++)
+            {
+
+                arraySouvenir[i].DisplayInformationSouvenir();
+                arraySouvenir[i].DisplayInformationManufacturer();
+            }
+            
+
+        }
+    }
+}
